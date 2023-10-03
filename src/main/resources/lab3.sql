8068254786db735c94540916c7a3bf9c32c9e8c7
Gersheli <victorshunko@mail.ru>
2023-10-03 18:51:58 +0300

    halved lab4

--- src/main/resources/lab3.sql
+++ src/main/resources/lab3.sql
@@ -1,18 +1,47 @@
+-- РАБОТА С ТАБЛИЦЕЙ Author
 select name, surname from author where country='';
 update author set country='' where name='' and surname='';
 delete from author where name='' and surname='';
 
+-- РАБОТА С ТАБЛИЦЕЙ Genre
 select name from genre where is_fiction=false;
 delete from genre where name='';
 
+-- РАБОТА С ТАБЛИЦЕЙ Book
 select title, series from book where author_id=1;
 update book set genre_id='' where series='';
 delete from book where id=1;
 
+-- РАБОТА С ТАБЛИЦЕЙ Recommendation
 select from recommendation where author_id=1;
 delete from recommendation where book_id=1 and author_id=1;
 
+-- РАБОТА С ТАБЛИЦЕЙ User
 select from "User";
 update "User" set is_admin=true where id=1;
 delete from "User" where id=1;
 
+-- РАБОТА С ТАБЛИЦЕЙ UserBook
+select from UserBook where user_id=1 and book_id=1;
+update UserBook set is_read=true and is_demanded=true where user_id=1 and book_id=1;
+delete from UserBook where user_id=1 and book_id=1;
+
+-- РАБОТА С ТАБЛИЦЕЙ Quote
+SELECT id, contents FROM Quote;
+UPDATE Quote SET contents = 'Updated quote' WHERE id = 1;
+DELETE FROM Quote WHERE id = 1;
+
+-- РАБОТА С ТАБЛИЦЕЙ BookReview
+SELECT id, rating, contents FROM BookReview;
+UPDATE BookReview SET rating = 80 WHERE id = 1;
+DELETE FROM BookReview WHERE id = 1;
+
+-- РАБОТА С ТАБЛИЦЕЙ Publisher
+SELECT id, name, address FROM Publisher;
+UPDATE Publisher SET phone = '987-654-3210' WHERE id = 1;
+DELETE FROM Publisher WHERE id = 1;
+
+-- РАБОТА С ТАБЛИЦЕЙ BookIssue
+SELECT id, year, quantity, price FROM BookIssue;
+UPDATE BookIssue SET price = 24.99 WHERE id = 1;
+DELETE FROM BookIssue WHERE id = 1;
--- src/main/resources/lab4.sql
+++ src/main/resources/lab4.sql
@@ -1,15 +1,45 @@
+-- Несколько условий (см. lab3)
 select * from author where name='' and surname='';
 
+-- Вложенные конструкции
+select name, surname from author
+where name in (select name from author where id > 3);
+
+-- Join-ы
 select b.title from book b
 join author a on b.author_id = a.id
 where a.name = '' and a.surname = '';
 
-select name, surname from author
-where name in (select name from author where id > 3);
+SELECT q.id, q.contents, b.title
+FROM Quote q JOIN Book b
+ON q.book_id = b.id;
 
+SELECT br.id, u.login, b.title, br.rating, br.contents
+FROM BookReview br
+JOIN "User" u ON br.user_id = u.id
+JOIN Book b ON br.book_id = b.id;
+
+SELECT bi.id, b.title, p.name, bi.year, bi.quantity, bi.price
+FROM BookIssue bi
+JOIN Book b ON bi.book_id = b.id
+JOIN Publisher p ON bi.publisher_id = p.id;
+
+-- Все виды join-ов
 select title, series, name, is_fiction from book b join genre g on g.id = b.genre_id;
 select title, series, name, is_fiction from book b left outer join genre g on g.id = b.genre_id;
 select title, series, name, is_fiction from genre g right outer join book b on b.genre_id = g.id;
 select title, series, name, is_fiction from book b full outer join genre g on b.genre_id = g.id;
 select title, series, name, is_fiction from book cross join genre;
 select t1.title, t1.id, t1.author_id, t2.id, t2.author_id from book t1 join book t2 on t1.id = t2.author_id;
+
+-- GROUP BY, HAVING, UNION и агрегирующие функции
+SELECT COUNT(genre_id) FROM book;
+SELECT SUM(rating) FROM bookreview;
+SELECT AVG(rating) FROM bookreview;
+SELECT MIN(rating) FROM bookreview;
+SELECT MAX(rating) FROM bookreview;
+
+SELECT column, COUNT(*) FROM table GROUP BY column;
+
+SELECT column, COUNT(*) FROM table GROUP BY column
+HAVING COUNT(*) > 10;
